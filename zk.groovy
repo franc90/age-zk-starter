23ebd7a8c7de00551fd89d64b6620aaffacfc647
franc90 <azarnowski1@gmail.com>
2016-08-03 23:25:13 +0200

    Add Zookeeper 3.4.8 + management script

---
+++ zk.groovy
@@ -0,0 +1,95 @@
+#!/usr/bin/env groovy
+import java.util.logging.FileHandler
+
+import static Constants.*
+
+class Constants {
+    static final sourceDir = 'zookeeper'
+    static final zookeeperArchive = 'zookeeper-3.4.8.tar.gz'
+    static final targetDir = '/tmp/zookeeper'
+    static final confFile = 'conf/zoo.cfg'
+    static final startScript = targetDir + '/bin/zkServer.sh'
+    static final stopScript = targetDir + '/bin/zkServer.sh'
+    static final cliScript = targetDir + '/bin/zkCli.sh'
+}
+
+def cleanup(args) {
+    def cli = new CliBuilder(usage: 'cleanup.groovy -[cdhls]')
+
+    cli.with {
+        s longOpt: 'start', 'Start Zookeeper'
+        c longOpt: 'close', 'Stop Zookeeper'
+        l longOpt: 'logs', 'Delete all log files'
+        d longOpt: 'delete', 'Delete all Zookeeper files'
+        h longOpt: 'help', 'Show help'
+    }
+
+    def options = cli.parse(args)
+
+    if (!options || args.length == 0 || options.h) {
+        cli.usage()
+        return
+    }
+
+    if (options.c || options.l || options.d) {
+        println 'Stop Zookeeper'
+        stopZookeeper()
+    }
+
+    if (options.d) {
+        println 'Delete all Zookeeper files'
+        deleteZookeeperFiles()
+    }
+
+    if (options.l) {
+        println 'Delete all log files'
+        deleteLogFiles();
+    }
+
+    if (options.s) {
+        println 'Start Zookeeper'
+        startZookeeper()
+    }
+}
+
+static def exists(String filePath) {
+    File file = new File(filePath)
+    return file.exists()
+}
+
+static def notExists(String filePath) {
+    !exists(filePath)
+}
+
+def stopZookeeper() {
+    if (notExists(stopScript)) {
+        println "${stopScript} does not exist"
+        return
+    }
+
+    Process p = "${stopScript} stop".execute()
+    println p.text
+    while (p.alive) {
+        print "$p is still alive. Waiting another 200ms"
+        sleep(200)
+    }
+}
+
+def deleteZookeeperFiles() {
+    if (notExists(targetDir)) {
+        println "${targetDir} does not exist"
+        return
+    }
+
+    new File(targetDir).deleteDir()
+}
+
+def deleteLogFiles() {
+    println 'Not implemented'
+}
+
+def startZookeeper() {
+    println 'Not implemented'
+}
+
+cleanup(args)
